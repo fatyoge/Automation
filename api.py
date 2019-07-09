aa911ce755cb91e074107a4534da60bfecc3392f
fatyogurt <77950683@163.com>
2019-07-09 18:05:32 +0800

    Update api.py

--- api.py
+++ api.py
@@ -1,19 +1,32 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api ,reqparse, fields, marshal_with, marshal
 from db_connector import *
+import json
+from flask_restful import reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
 todos = {}
-
-class APIQuery(Resource):
-    def get(self, table):
-        cf = ConnectorFactory()
-        db_conn = cf.get_or_createConnector(server_name="presto_yz_targetmetric_dim")
-        return {table: db_conn.query_table(table)}
-
-api.add_resource(APIQuery, '/api/query/<string:table>')
+class Query(Resource):
+    
+    #@marshal_with(resource_fields, envelope='resource')
+    def get(self, table_name):
+        parser = reqparse.RequestParser()
+        parser.add_argument('_size', type=int, dest='limit')
+        parser.add_argument('_fields', type=str, dest='fields')
+        parser.add_argument('_where', type=str, dest='whereclause')
+        parser.add_argument('_sort', type=str, dest='order_by')
+        db_conn = ConnectorFactory.curr_connector
+        args = parser.parse_args()
+        print(table_name)
+        print(args)
+        #result = db_conn.query_table(table_name, **args)
+        #print(result)
+        #return json.dumps(marshal(db_conn.query_table(table_name, **args), resource_fields))
+        return db_conn.query_table(table_name, **args)
+    
+api.add_resource(Query, '/api/<string:table_name>/')
 
 #class Flask_Rest_Server():
 #    def __init__(self):
@@ -48,6 +61,6 @@ if __name__ == '__main__':
         ,"schema": "yz_targetmetric_dim"
         }
     }
-    cf = ConnectorFactory()
+    cf = ConnectorFactory
     db_conn = cf.get_or_createConnector(**presto_server)
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
